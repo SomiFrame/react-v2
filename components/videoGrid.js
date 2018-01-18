36eabc94b3615a79a72832b13aeedfe765d1ff42
frame <frame@moonjoy.net>
2018-01-18 17:37:32 +0700

    update

---
+++ components/videoGrid.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'next/link'
+import Css from '../styles/c-videoGrid.scss'
+class VGrid extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <style dangerouslySetInnerHTML={{ __html: Css }} />
+                <ul className="video-grid">
+                    {
+                        this.props.videoGrids.map((item, index) =>
+                            <li key={index} className="grid-item">
+                                <Link href={item.href}>
+                                    <a>
+                                        <img src={item.img_url}/>
+                                        <div className="grid-status">
+                                        {'正在直播'}
+                                        </div>
+                                        <div className="grid-text-container">
+                                        </div>
+                                    </a>
+                                </Link>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+export default VGrid 
