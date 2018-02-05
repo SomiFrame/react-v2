4117227abf681fcaa3d4dfe139e867366b4817e5
frame <frame@moonjoy.net>
2018-02-05 17:33:32 +0700

    update

--- components/news.js
+++ components/news.js
@@ -1,11 +1,44 @@
-import {Component} from 'react'
+import { Component, Fragment } from 'react'
 import Css from '../styles/c-news.scss'
+
 class News extends Component {
-    render(){
+    constructor(props) {
+        super(props)
+        this.state = {
+            WebSocket: null,
+            news: false,
+            data: null
+        }
+    }
+    componentDidMount() {
+        this.creatSocket()
+    }
+    creatSocket() {
+        if ("WebSocket" in window) {
+            const ws = new window.WebSocket('wss://echo.websocket.org')
+            ws.onopen = (data) => {
+                ws.send('message to server')
+                this.setState({ WebSocket: ws })
+            }
+            ws.onmessage = (data) => {
+                console.log(data, 'from servere massage')
+                this.setState({news: true})
+            }
+            ws.onerror = (data) => {
+                console.log(data, 'from servere massage')
+            }
+        }
+    }
+    render() {
         return (
             <div>
-                <style dangerouslySetInnerHTML={{ __html: Css }} />
-                <div className="news-block">为您推荐了{7}部视频</div>
+                {
+                    this.state.news &&
+                    <Fragment>
+                        <style dangerouslySetInnerHTML={{ __html: Css }} />
+                        <div className="news-block">为您推荐了{7}部视频</div>
+                    </Fragment>
+                }
             </div>
         )
     }
